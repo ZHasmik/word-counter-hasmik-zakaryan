9146501344442d024e9ca1a193c4fd90112777c2
ZHasmik <125693020+ZHasmik@users.noreply.github.com>
2023-03-05 16:58:20 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const userString = document.querySelector("#userString");
+
+const words = document.querySelector("#words");
+const letters = document.querySelector("#letters");
+const sentences = document.querySelector("#sentences");
+
+function countWords(){
+
+}
+let amountOfLetter
+
+
+
+function counter() {
+    countWords();
+    countLetters();
+    countSentences();
+
+
+}
