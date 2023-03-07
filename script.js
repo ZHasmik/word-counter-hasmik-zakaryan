53634d35d5f5c95f69c3e417138110d8f47236d5
ZHasmik <125693020+ZHasmik@users.noreply.github.com>
2023-03-07 18:16:01 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const userString = document.querySelector("#userString");
-
+const textarea = document.querySelector(".user__string");
+let arr = textarea.innerHTML.split(' ');
+console.log(arr.length);
 const words = document.querySelector("#words");
+const spaces = document.querySelector("#spaces");
 const letters = document.querySelector("#letters");
 const sentences = document.querySelector("#sentences");
 
-function countWords(){
-
-}
-let amountOfLetter
 
 
-
-function counter() {
-    countWords();
-    countLetters();
-    countSentences();
-
-
-}
+// function countSpaces() {
+//     let counter = 0;
+//     for (let el of arr){
+//         if (el !== null) {
+//             continue;
+//         } else {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+// console.log(countSpaces());
